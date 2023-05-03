18efdc49c8a4ea949406c687e1ffbc024a3e4272
Deleranax <Deleranax@dwight-studio.fr>
2023-05-03 19:54:22 +0200

    Updating tiles

---
+++ src/main/resources/tileset/Trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Trees" tilewidth="96" tileheight="128" tilecount="320" columns="10">
+ <image source="../entities/plants/fruit-trees.png" width="1024" height="4096"/>
+</tileset>
